31cc0d6da7c7abd91aa3d10fd0ebcc4083bbbb21
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2022-09-04 00:52:19 -0400

    first

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import CenterView from "./lighttable/CenterView.svelte";
+</script>
+
+<main>
+  <div>
+    <CenterView />
+  </div>
+</main>
---
+++ src/lighttable/CenterView.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {
+        textField
+    } from "material-components-web/index.d.ts";
+
+    const PrintText = () => {
+        const queryText = new textField.MDCTextField(document.querySelector('.queryBar')).value;
+        console.log(queryText);
+    }
+</script>
+
+<label class="mdc-text-field queryBar">
+    <span class="mdc-text-field__ripple"></span>
+    <span class="mdc-floating-label" id="my-label-id">Enter query...</span>
+    <input class="mdc-text-field__input" type="text" aria-labelledby="my-label-id">
+    <span class="mdc-line-ripple"></span>
+</label>
+
+<style lang="scss">
+  @use '@material/textfield';
+    .queryBar{
+        @include textfield.core-styles(   );
+    }
+</style>
